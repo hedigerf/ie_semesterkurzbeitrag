4106ff2d7a9e3cf15ac26d0002e9e02cbb560742
Fabian Hediger <hedigerf@gmail.com>
2015-10-09 21:58:24 +0200

    implement prototype frequency map

--- Program.fs
+++ Program.fs
@@ -13,13 +13,34 @@ module Main =
         let sepAry = [| ' '; ','|]
         Array.toList (text.Split sepAry)
 
+    let rec addEntry entry (acc:Map<string,int>) =
+        match acc.TryFind(entry) with
+        | Some(count) -> acc.Remove(entry).Add(entry,count + 1)
+        | None -> acc.Add(entry,1)
+
+    let rec searchDocument (document:DocumentItem) (acc:Map<string,int>) =
+        match document.TokenizedText with
+        | head::tail -> addEntry head acc
+        | [] -> acc
+
+    let rec buildFrequencyMap (documentItems:List<DocumentItem>) (acc:Map<string,int>) =
+        match documentItems with
+        | head::tail -> searchDocument head acc
+        | [] -> acc
+
     [<EntryPoint>]
     let main argv = 
+        let stopWatch = System.Diagnostics.Stopwatch.StartNew()
         let collection =Collection.Load "../../Resources/irg_collection.trec"
-        let documentItems= Array.toList(collection.Docs |> Array.map (fun doc ->
-            (doc.RecordId,DocumentItem(doc.RecordId,doc.Text,tokenize(doc.Text)))))
-        let documentsById = Map.ofList documentItems
-        documentItems |> List.iter (fun doc -> printfn "%s" (snd doc).OriginalText)
+        let documentItems = Array.toList(collection.Docs |> Array.map (fun doc ->
+            DocumentItem(doc.RecordId,doc.Text,tokenize(doc.Text))))
+        let frequencyMap = buildFrequencyMap documentItems Map.empty<string,int>
+        //let documentItems= Array.toList(collection.Docs |> Array.map (fun doc ->
+        //    (doc.RecordId,DocumentItem(doc.RecordId,doc.Text,tokenize(doc.Text)))))
+        //let documentsById = Map.ofList documentItems
+        //documentItems |> List.iter (fun doc -> printfn "%s" (snd doc).OriginalText)
+        stopWatch.Stop()
+        printfn "%f" stopWatch.Elapsed.TotalMilliseconds
         0;
-        //0 // return an integer exit code
+        //0 // return an /integer exit code 
 
