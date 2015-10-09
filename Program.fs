88529b273135225b991b2a41e663f57b870c1f88
Fabian Hediger <hedigerf@gmail.com>
2015-10-09 11:55:22 +0200

    reading of xml file and type  to store docs

--- Program.fs
+++ Program.fs
@@ -1,7 +1,21 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
+﻿module Main
+open System.IO
+open System
+open FSharp.Data
+open System.Xml.Linq
+
+type Collection = XmlProvider<"Resources/irg_collection.trec">
+
+
 [<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+let main argv =
+   // let parsedFile = File.ReadAllLines("../../Resources/irg_collection.trec")
+    //let parsedStr = Array.reduce (fun acc item -> acc + item) parsedFile 
+    //let test = Collection.Parse "Resources/irg_collection.trec"   
+    let collection=Collection.Load "../../Resources/irg_collection.trec"
+    for doc in collection.Docs do
+          printfn "%s" doc.Text
+
+    0;
+    //0 // return an integer exit code
 
---
+++ TypeDomain.fs
@@ -0,0 +1,9 @@
+﻿namespace Semesterkurzbeitrag
+open System
+
+[<AutoOpen>]
+module TypeDomain =
+
+    type DocumentItem(recordId,tokenizedText) =
+        member this.RecordId = recordId
+        member this.TokenizedText = tokenizedText 
